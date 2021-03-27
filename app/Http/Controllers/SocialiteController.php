189e67e0d15c146142e4bfc41dee3a2bac119811
miltonfel <miltonfel@brafel.com.br>
2021-03-27 10:14:09 -0300

    auth ok inicio logica oauth

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class SocialiteController extends Controller
 {
@@ -14,12 +15,27 @@ class SocialiteController extends Controller
         $email = $user->getEmail();
         $foto = $user->getAvatar();
 
-        $tipousuario = confereCadastro($email);
-        return $tipousuario;
+        $existeCadastro = confereCadastro($email);
+        if ($existeCadastro == false){
+            return "Iniciar casdastro novo";
+        }
+        else return "Linkar ou login";
+        
     }
 
 }
 
 function confereCadastro($email){
-    return "Paciente";
+    $user = User::with(['convenio'])->where('email', $email)->get();
+    if (isset ($user[0]->email)) return $user; //email já existe no cadastro de usuário
+    else return false; //e-mail não existe no cadastro
+    /*
+        if ($user[0]->password = 'usuario sem acesso'){
+            return 'Linkar usuario com pre cadastro';
+        }
+        else {
+            return 'Usuário já existente, prosseguir para login';
+        }
+    }
+    else return "Usuário não cadastrado";*/
 }
